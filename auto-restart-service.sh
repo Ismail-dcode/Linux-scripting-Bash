3c07c7947e5be34e14d2b529b53fe435639f0c5d
Ismail-dcode <ismailpromt2i@gmail.com>
2026-02-17 17:09:01 +0000

    Updated

--- auto-restart-service.sh
+++ auto-restart-service.sh
@@ -7,7 +7,7 @@ if systemctl is-active --quiet $SERVICE
 then
     echo "$SERVICE is running"
 else
-    echo "$SERVICE is NOT running"
+    echo "$SERVICE  is NOT running"
     echo "Restarting $SERVICE..."
     systemctl restart $SERVICE
 fi
