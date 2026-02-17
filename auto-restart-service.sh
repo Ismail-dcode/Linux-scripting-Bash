581dadb5184f482470499f621f14bd4c97a04d02
Ismail-dcode <ismailpromt2i@gmail.com>
2026-02-17 17:07:11 +0000

    auto start service script

---
+++ auto-restart-service.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+read -p "Service name ? : " SERVICE 
+
+if systemctl is-active --quiet $SERVICE
+then
+    echo "$SERVICE is running"
+else
+    echo "$SERVICE is NOT running"
+    echo "Restarting $SERVICE..."
+    systemctl restart $SERVICE
+fi
+
