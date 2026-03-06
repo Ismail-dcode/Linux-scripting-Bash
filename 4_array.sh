4990ec2a295a8c132fae2dcab5e57cf8f8adfeae
Shaikh Ismail <ismailpromt2i@gmail.com>
2026-03-06 11:28:31 +0000

    new updaters

--- 4_array.sh
+++ 4_array.sh
@@ -5,4 +5,11 @@ myArray=(12 100.00 nice "JP-Morgan")
 echo "${myArray[3]}"
 echo "${myArray[2]}"
 echo "${myArray[1]}"
-echo "${myArray[*]}"
+myArray[1]=1000.67
+myArray+=(1000 umask "wow")
+
+echo "${myArray[*]:1:5}"
+
+
+
+
